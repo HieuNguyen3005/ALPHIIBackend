2f51860831de9ccee8502d0b035408a5a23bf497
HieuNguyen3005 <huuhieunguyen3005@gmail.com>
2024-07-24 16:10:24 +0700

    Macbook 24/07/2024 add result class

---
+++ Models/DTO/results.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace ALPHII.Models.DTO
+{
+	public class results
+	{
+        public string color { get; set; }
+
+        public string image_base64 { get; set; }
+
+        public string image_base64_display { get; set; }
+	}
+}
+
